8c4430e169b48febbeea71374cf9ba58a97fff82
Valen <99224577+Valenby@users.noreply.github.com>
2023-04-29 10:43:21 -0500

    Revert "listoMongodb"
    This reverts commit 3cc60f7fdb1a22a0202b960f37b121ed463ea6cb.

--- appExpress/middlewaresJoi.js
+++ appExpress/middlewaresJoi.js
@@ -2,23 +2,23 @@ const Joi = require('joi');
 
 // schema para la validación
 const createBookSchema = Joi.object({
-    name: Joi.string().required(),
+    title: Joi.string().required(),
     author: Joi.string().required(),
     category: Joi.string().required(),
     description: Joi.string().required(),
     price: Joi.number().integer().min(0).required(),
     pages: Joi.number().integer().min(0).required(),
-    availlableUnits: Joi.number().integer().min(0).required(),
+    unit: Joi.number().integer().min(0).required(),
 });
 
 const updateBookSchema = Joi.object({
-    name: Joi.string(),
+    title: Joi.string(),
     author: Joi.string(),
     category: Joi.string(),
     description: Joi.string(),
     price: Joi.number().integer().min(0),
     pages: Joi.number().integer().min(0),
-    availlableUnits: Joi.number().integer().min(0),
+    unit: Joi.number().integer().min(0),
 });
 
 const validateMiddlewareCreate  = (req, res, next) => {
